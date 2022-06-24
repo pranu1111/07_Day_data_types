ca34f6839e8fc7529b01d3173f795d012c723baa
Pranita kamble <107885176+pranu1111@users.noreply.github.com>
2022-06-25 02:34:38 +0530

    Add files via upload

---
+++ data_type.py
@@ -0,0 +1,18 @@
+a=5  
+b=5 
+c=10
+print(a,b,c)
+p=1.02; q=6.50; r=9.20
+print(p,q,r)
+print("Addition of p,q,r=",p+q+r)
+p="python"
+print(p,"is popular language")
+h='hello'; w='world'
+print(h+' '+ w)
+# print(h.w)
+print(type(h))
+print(type(q))
+print(p.isidentifier())
+str1='    '
+print(str1.isspace())
+print(type(a))
